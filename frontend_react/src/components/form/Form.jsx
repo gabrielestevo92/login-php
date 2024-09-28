5cce03394adbd9b39beea85e980b18aa4efd7cb5
Gabriel Estevão Nunes <gabrielestevo92@gmail.com>
2024-09-28 10:38:43 -0300

    feat: Adicionando validação de formulario para o campo de email

--- frontend_react/src/components/form/Form.jsx
+++ frontend_react/src/components/form/Form.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import './form.css'
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+// Componente responsavel pela estrutura de login
 const Form = () => {
+    const [email, setEmail] = useState("");
+    
+    // Validação de formulario
+    const { register, handleSubmit, formState: {errors} } = useForm()
+    
+    //Função chamada ao enviar o formulario
+    const onSubmit = (event) => {
+        alert(event.email)
+    }
     return(
-        <form action="">
-            <h1>Please sign in</h1>    
-            <input id="email" type="text" placeholder="Email address" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Please sign in</h1>  
+              
+              <input
+                id="email"
+                type="email"
+                placeholder="Email address"
+                {...register("email", {
+                    required: "O e-mail é obrigatório",                    
+                    pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Formato de e-mail inválido"
+                    }
+                })}
+                />
+                {errors.email && <p>{errors.email.message}</p>}
             <div className="remember-me">
                 <input type="checkbox" id="remember"/>
                 <label htmlFor="remember">Remember me</label>
             </div>
-            <input id="next" type="submit" name="next"/>
+           <input type="submit" value="Next" id="next" />
             
         </form> 
     )
